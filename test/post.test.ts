d5e2513b47b8c70f49a477fa4de08412322655f7
xingobar <gary840227@gmail.com>
2021-07-17 21:51:18 +0800

    test: 取得文章測試
    1. 取得文章測試

--- test/post.test.ts
+++ test/post.test.ts
@@ -1,14 +1,23 @@
 import { expect, assert } from "chai";
 import { getConnection } from "typeorm";
-import { api, createUser } from "./global.test";
+import { api, createUser, createUserAndLogin, getCurrentUser, fakeLogin } from "./global.test";
 import { Post, PostStatus } from "entity/post.entity";
 import { User } from "entity/user.entity";
 import { factory, useRefreshDatabase } from "typeorm-seeding";
 
 let post: Post | undefined;
 
-export const createPost = async ({ status = PostStatus.PUBLISH }: { status: PostStatus }) => {
-  const user = await createUser();
+export const createPost = async ({
+  status = PostStatus.PUBLISH,
+  user = undefined,
+}: {
+  status: PostStatus;
+  user?: User | undefined;
+}): Promise<Post | undefined> => {
+  if (!user) {
+    user = await createUser();
+  }
+
   await getConnection("test")
     .createQueryBuilder()
     .insert()
@@ -22,6 +31,8 @@ export const createPost = async ({ status = PostStatus.PUBLISH }: { status: Post
     .execute();
 
   post = await getConnection("test").getRepository(Post).createQueryBuilder().where("userId = :userId", { userId: user?.id }).getOne();
+
+  return post;
 };
 
 describe("get post index ", () => {
@@ -54,5 +65,17 @@ describe("get post index ", () => {
     await createPost({ status: PostStatus.PUBLISH });
     const res = await api.get("/posts").expect(200);
     assert.equal(res.body.length, 1);
+  });
+});
+
+describe("show post", () => {
+  it("post not found", () => {
+    api.get("/posts/1").expect(404);
+  });
+
+  it("get post", async () => {
+    const post = await createPost({ status: PostStatus.PUBLISH });
+
+    api.get(`/posts/${post?.id}`).expect(200);
   });
 });
