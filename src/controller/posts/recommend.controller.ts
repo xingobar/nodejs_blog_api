51972d22f9eed45d0d4e025da865b61f1e376873
xingobar <gary840227@gmail.com>
2021-07-18 10:09:49 +0800

    test: 受歡迎文章測試
    1. 受歡迎文章測試

--- src/controller/posts/recommend.controller.ts
+++ src/controller/posts/recommend.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Request, Response } from "@decorators/express";
 import { Container } from "typedi";
 import PostService from "service/post.service";
 import PostResource from "resource/post.resource";
+import NotFoundException from "exception/notfound.exception";
 
 @Controller("/posts")
 export default class RecommendController {
@@ -9,6 +10,11 @@ export default class RecommendController {
   @Get("/:postId/popularity")
   public async popularity(@Request() req: any, @Response() res: any) {
     const postService: PostService = Container.get(PostService);
+
+    const post = await postService.findByIdWithPublished(req.params.postId);
+    if (!post) {
+      throw new NotFoundException();
+    }
 
     const posts = await postService.getPopularity(req.params.postId);
 
---
+++ test/posts/recommend.test.ts
@@ -0,0 +1,66 @@
+import Faker from "faker";
+import { api, createUser } from "../global.test";
+import { Post, PostStatus } from "entity/post.entity";
+import { User } from "entity/user.entity";
+import { getConnection } from "typeorm";
+import { assert } from "chai";
+import { useRefreshDatabase } from "typeorm-seeding";
+
+export const createPost = async ({ status = PostStatus.PUBLISH, user = undefined }: { status: PostStatus; user?: User | undefined }) => {
+  if (!user) {
+    user = await createUser();
+  }
+
+  await getConnection("test")
+    .createQueryBuilder()
+    .insert()
+    .into(Post)
+    .values({
+      title: "demo",
+      body: "demo",
+      userId: user?.id,
+      status,
+    })
+    .execute();
+
+  const post = await getConnection("test")
+    .getRepository(Post)
+    .createQueryBuilder()
+    .where("userId = :userId", { userId: user?.id })
+    .getOne();
+
+  return { user, post };
+};
+
+describe("popularity post test", () => {
+  beforeEach(async () => {
+    await useRefreshDatabase({ configName: "test.ormconfig.json", connection: "test" });
+  });
+
+  it("post not found", (done) => {
+    api
+      .get("/posts/1/popularity")
+      .expect(404)
+      .end((err, res) => {
+        done();
+      });
+  });
+
+  it("no popularity post", async () => {
+    const { user, post } = await createPost({ status: PostStatus.PUBLISH });
+
+    const res = await api.get(`/posts/${post?.id}/popularity`).expect(200);
+
+    assert.equal(res.body.length, 0);
+  });
+
+  it(" popularity post", async () => {
+    const { user, post } = await createPost({ status: PostStatus.PUBLISH });
+
+    await createPost({ status: PostStatus.PUBLISH });
+
+    const res = await api.get(`/posts/${post?.id}/popularity`).expect(200);
+
+    assert.equal(res.body.length, 1);
+  });
+});
