925063bf8ff6360fd6c88b8811852de54a9b8831
xingobar <gary840227@gmail.com>
2021-07-18 13:30:54 +0800

    docs: 取得使用者資訊 swagger
    1. 取得使用者資訊 swagger

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,18 +1,37 @@
 import { Request, Response, Get, Controller } from "@decorators/express";
-import { injectable } from "inversify";
 import AuthenticateMiddleware from "middleware/authenticate.middleware";
 import UserResource from "resource/user.resource";
-import { ApiPath } from "swagger-express-ts";
+
+// swagger
+import { ApiPath, ApiOperationGet, SwaggerDefinitionConstant } from "swagger-express-ts";
+import { injectable } from "inversify";
+import { interfaces } from "inversify-express-utils";
 
 @ApiPath({
   path: "/users",
   name: "Users",
+  description: "使用者資訊",
 })
 @injectable()
 @Controller("/users")
-export default class UserController {
+export default class UserController implements interfaces.Controller {
   public static TARGET_NAME: string = "UserController";
 
+  @ApiOperationGet({
+    path: "/",
+    security: {
+      authorization: ["Bearer <token>"],
+    },
+    summary: "取得使用者資訊",
+    description: "取得使用者資訊",
+    responses: {
+      200: {
+        description: "成功",
+        type: SwaggerDefinitionConstant.Response.Type.OBJECT,
+        model: "UserResponse",
+      },
+    },
+  })
   @Get("/", [AuthenticateMiddleware])
   public async index(@Request() req: any, @Response() res: any) {
     const userResource = new UserResource(req.session.user);
